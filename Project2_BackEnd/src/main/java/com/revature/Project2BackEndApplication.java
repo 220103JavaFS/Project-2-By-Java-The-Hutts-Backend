f09996200f3047a6ff2735dacdc64a633b3387e1
hm9811 <hm981198@gmail.com>
2022-02-10 19:21:27 -0800

    created Database connection

--- Project2_BackEnd/src/main/java/com/revature/Project2BackEndApplication.java
+++ Project2_BackEnd/src/main/java/com/revature/Project2BackEndApplication.java
@@ -1,4 +1,4 @@
-package com.revature.Project2_BackEnd;
+package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
