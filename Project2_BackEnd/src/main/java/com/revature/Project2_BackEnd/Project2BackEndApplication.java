582f75277e571a6b4e0f1b25f11fcd60f2f86471
hm9811 <hm981198@gmail.com>
2022-02-10 18:25:54 -0800

    created backends gradle

---
+++ Project2_BackEnd/src/main/java/com/revature/Project2_BackEnd/Project2BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Project2_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2BackEndApplication.class, args);
+	}
+
+}
---
+++ Project2_BackEnd/src/test/java/com/revature/Project2_BackEnd/Project2BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Project2_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
