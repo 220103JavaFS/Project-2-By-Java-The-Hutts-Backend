b61bae8d198a6e7037b9a9176c35c8b36aaaf392
hm9811 <hm981198@gmail.com>
2022-02-10 19:31:34 -0800

    added Constructor, getter, setter, equals, hashCode, and toString

--- Project2_BackEnd/src/main/java/com/revature/models/Users.java
+++ Project2_BackEnd/src/main/java/com/revature/models/Users.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Entity
 public class Users {
@@ -12,23 +13,116 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
-    @NotNull
+    @Column(nullable = false)
     private String firstname;
 
-    @NotNull
+    @Column(nullable = false)
     private String lastname;
 
-    @Column(unique=true)
-    @NotNull
+    @Column(unique=true, nullable = false)
     private String username;
 
-    @Column(unique=true)
-    @NotNull
+    @Column(unique=true, nullable = false)
     private String email;
 
-    @NotNull
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.ORDINAL)
     private ArrayList<UserPreferences> userPreferences;
+
+    public Users() {
+    }
+
+    public Users(int userId, String firstname, String lastname, String username, String email, String password, ArrayList<UserPreferences> userPreferences) {
+        this.userId = userId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userPreferences = userPreferences;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<UserPreferences> getUserPreferences() {
+        return userPreferences;
+    }
+
+    public void setUserPreferences(ArrayList<UserPreferences> userPreferences) {
+        this.userPreferences = userPreferences;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return userId == users.userId && Objects.equals(firstname, users.firstname) && Objects.equals(lastname, users.lastname) && Objects.equals(username, users.username) && Objects.equals(email, users.email) && Objects.equals(password, users.password) && Objects.equals(userPreferences, users.userPreferences);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, firstname, lastname, username, email, password, userPreferences);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userId +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", userPreferences=" + userPreferences +
+                '}';
+    }
 }
