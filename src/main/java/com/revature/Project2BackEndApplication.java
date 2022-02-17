88bbf68a1a88d4bb6e09e4a412e8a72ec817857f
michael <mludwig0626@aol.com>
2022-02-17 17:25:59 -0500

    User preference storage successful

--- src/main/java/com/revature/Project2BackEndApplication.java
+++ src/main/java/com/revature/Project2BackEndApplication.java
@@ -1,10 +1,16 @@
 package com.revature;
-
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Project2BackEndApplication {
+
+	//Import logger
+	private static final Logger mylog = LoggerFactory.getLogger(Project2BackEndApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2BackEndApplication.class, args);
